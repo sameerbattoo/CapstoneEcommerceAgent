92605b30b0d62132cea2b2bf9aac84bd7478911c
sameerbattoo <battoo.sameer@live.com>
2025-12-16 15:34:27 -0800

    Update deploy_agent.sh

--- deploy_agent.sh
+++ deploy_agent.sh
@@ -6,16 +6,17 @@ set -e
 uv run agentcore configure -e main.py --vpc \
   --subnets subnet-0d924ff443cebd41d,subnet-0463efcbc95e2d5e1 \
   --security-groups sg-06d560db4091b375c \
-  --name capstone_ecommerce_agent_cognito \
+  --name capstone_ecomm_agent \
   --region us-west-2 \
   --execution-role arn:aws:iam::175918693907:role/CapstoneECommerce-agentcore-execution-role \
   --disable-memory \
+  --deployment-type container \
   --authorizer-config '{"type":"customJWTAuthorizer","discoveryUrl":"https://cognito-idp.us-west-2.amazonaws.com/us-west-2_5cqnrBvAg/.well-known/openid-configuration","allowedClients":"2nc8c09npb6vru63mgfl7kiqb1"}' \
-  --request-header-allowlist Authorization,X-Amzn-Bedrock-AgentCore-Runtime-Custom-Session-Id \
+  --request-header-allowlist Authorization,X-Amzn-Bedrock-AgentCore-Runtime-Session-Id \
   --non-interactive
 
 uv run agentcore launch \
 	--env AWS_REGION="us-west-2" \
-    --env AWS_SECRET_NAME="capstone-ecommerce-agent-config"
+  --env AWS_SECRET_NAME="capstone-ecommerce-agent-config"
 
 echo "Agent deployment complete!"
